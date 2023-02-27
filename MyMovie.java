4797f2088590cf82ee53bcdc403e88f009536a28
ahmidev <ahmid.aitouali@gmail>
2023-02-27 13:54:57 +0100

    quete java for variable

---
+++ MyMovie.java
@@ -0,0 +1,15 @@
+class MyMovie {
+
+    public static void main(String[] args) {
+        // afficher les variable dans le terminal
+	String movieTitle = "Indiana Jones and the Last Crusade";
+	boolean alreadySeen = false;
+	int releaseYear = 1989;
+	double movieRating = 8.2;
+	System.out.println("Movie name : "+ movieTitle);
+	System.out.println("Already seen ? : "+ alreadySeen);
+	System.out.println("Release year : "+releaseYear);
+	
+        System.out.println("Note : "+ movieRating);
+    }
+}
